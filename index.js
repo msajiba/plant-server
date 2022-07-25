76c560759251dde451ed0447baf83b58721a21bc
sajib-ahmed <onlineclass82@gmail.com>
2022-07-25 16:55:10 +0600

    update api create and quantity deliver

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -24,19 +24,57 @@ async function run(){
         await client.connect();
         const plantCollections = client.db('product').collection('items');
         
+        //GET PLANTS INVENTORY ITEM
         app.get('/plants', async(req, res) => {
             const query = {};
             const cursor = plantCollections.find(query);
             const plants = await cursor.toArray();
             res.send(plants);
-        })
+        });
 
+        //GET SINGLE PLANT INVENTORY ITEM
+        app.get('/plant/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const plant = await plantCollections.findOne(query);
+            res.send(plant);
+        });
+
+        // ALL PLANT POST 
+        app.post('/plant', async(req, res) =>{
+            const item = req.body.items;
+            const result = await plantCollections.insertOne(item);
+            res.send(result);
+        });
+        
+        //PLANT QUANTITY UPDATE
+        app.put('/plant/:id', async(req, res)=> {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+
+            console.log("quantity",updateQuantity);
+
+            const filter = {_id:ObjectId(id)};
+            const options = {upsert : true};
+
+            const updateDoc = {
+                $set : {
+                    quantity : updateQuantity.newQuantity
+                }
+            };
+
+            const updateFinalQuantity = await plantCollections.updateOne(filter, updateDoc, options);
+
+            const query = {_id: ObjectId(id)};
+            const plant=  await plantCollections.findOne(query);
+            res.send({updateFinalQuantity,plant});
+
+        });
+    
     }
     finally{}
-
 }
 run().catch(console.dir);
-
 
 
 
